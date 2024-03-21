deaca7d1ea026f2368f8dfa05b642347e051a591
CalleAlexanderson <calle.alexanderson@telia.com>
2024-03-21 01:42:06 +0100

    localstorage funkar nu

--- src/kurser.ts
+++ src/kurser.ts
@@ -1,15 +1,15 @@
 // json fil skapad med den data på kurser vi använt i tidigare kurs https://dahlgren.miun.se/ramschema_ht23.php
 import courses from "./kurser.json"; //import kod hittat här https://bobbyhadz.com/blog/typescript-import-json-file
 let g: any;
-let cc: string[];
+let f: string[] = [];
+let inStorage: boolean = true;
+
 if (localStorage.getItem("courses") != null) {
   g = localStorage.getItem("courses");
-
-  console.log(typeof g);
-  console.log(g);
-  console.log(g.split(","));
-  cc = g.split(",");
-  console.log(cc);
+  f = g.split(",");
+  console.log(f);
+} else {
+  inStorage = false;
 }
 
 let coursesArr: CourseInfo[] = [];
@@ -20,13 +20,30 @@ interface CourseInfo {
   progression: string;
   syllabus: string;
 }
-
-for (let index = 0; index < courses.length; index++) {
-  addCourseFromJson(index);
+console.log(inStorage);
+if (inStorage == false) {
+  for (let index = 0; index < courses.length; index++) {
+    addCourseFromJson(index);
+  }
+} else{
+  for (let index = 0; index < f.length; index=index+4) {
+    console.log('tar från storage ' + index);
+    addCourseFromStorage(index);
+  }
 }
 
 printCourses();
 console.log(coursesArr);
+
+function addCourseFromStorage(nr: number){
+  let kurs: CourseInfo = {
+    code: f[nr],
+    name: f[nr+1],
+    progression: f[nr+2],
+    syllabus: f[nr+3],
+  };
+  coursesArr.push(kurs);
+}
 
 function addCourseFromJson(nr: number) {
   let kurs: CourseInfo = {
@@ -66,33 +83,41 @@ function addCourse() {
 
 function printCourses() {
   let div = document.getElementById("courses") as HTMLDivElement;
-  console.log(div);
-
-  //lade till en if sats för annars för jag en varning att div kan vara null även om det funkar
-
+  let arr: string[] = [];
   div.innerHTML = "";
   for (let index = 0; index < coursesArr.length; index++) {
     let capsule: HTMLDivElement = document.createElement("div");
     let h3: HTMLHeadingElement = document.createElement("h3");
     h3.innerHTML = coursesArr[index].code;
+    arr.push(coursesArr[index].code);
 
     let p1: HTMLParagraphElement = document.createElement("p");
     p1.innerHTML = coursesArr[index].name;
+    arr.push(coursesArr[index].name);
 
     let p2: HTMLParagraphElement = document.createElement("p");
     p2.innerHTML = coursesArr[index].progression;
+    arr.push(coursesArr[index].progression);
 
     let a: HTMLAnchorElement = document.createElement("a");
     a.innerHTML = "studieplan";
     a.href = coursesArr[index].syllabus;
+    arr.push(coursesArr[index].syllabus);
+
     capsule.appendChild(h3);
     capsule.appendChild(p1);
     capsule.appendChild(p2);
     capsule.appendChild(a);
     div.appendChild(capsule);
   }
-  let ccS: string = cc.join();
-  console.log(ccS);
-  localStorage.setItem("courses", ccS);
-  console.log(localStorage.getItem("courses"));
+  console.log(coursesArr);
+  console.log("arr " + arr);
+  localStorage.setItem("courses", arr.join());
+  console.log("storage " + localStorage.getItem("courses"));
+  let b: any;
+  if (localStorage.getItem("courses") != null) {
+    b = localStorage.getItem("courses");
+
+    console.log(b.split(","));
+  }
 }
